93068ce9af8a5d3ff77a0408743d28a78920ca9c
hgjunior <hjunior@tech6group.com>
2025-03-26 12:18:55 -0300

    Include one more nested scope.

--- poc_scope_serviceprovider/Program.cs
+++ poc_scope_serviceprovider/Program.cs
@@ -26,34 +26,62 @@ app.MapGet("/test", (HttpContext httpContext) =>
     IFooSingleton foo = provider.GetService<IFooSingleton>()!;
     IBarScoped bar = provider.GetService<IBarScoped>()!;
 
-    foo.SetName("Foo 1");
-    bar.SetName("Bar 1");
+    foo.SetName("Foo 1 - HttpContext");
+    bar.SetName("Bar 1 - HttpContext");
 
     Console.WriteLine("Http Scope");
     Console.WriteLine(foo.GetName());
     Console.WriteLine(bar.GetName());
-    Console.WriteLine("----------");
+    Console.WriteLine("----------\n");
     
-    using (var scope = provider.CreateScope())
+    using (var scope1 = provider.CreateScope())
     {
-        IFooSingleton fooService = scope.ServiceProvider.GetService<IFooSingleton>()!;
-        IBarScoped barService = scope.ServiceProvider.GetService<IBarScoped>()!;
+        IFooSingleton fooService1 = scope1.ServiceProvider.GetService<IFooSingleton>()!;
+        IBarScoped barService1 = scope1.ServiceProvider.GetService<IBarScoped>()!;
 
-        Console.WriteLine("New Scope");
-        Console.WriteLine(fooService.GetName());
-        Console.WriteLine(barService.GetName());
+        Console.WriteLine("New Scope 1");
+        Console.WriteLine(fooService1.GetName());
+        Console.WriteLine(barService1.GetName());
+        Console.WriteLine("----------\n");
+        
+        fooService1.SetName("Foo 2");
+        barService1.SetName("Bar 2");
+        
+        using (var scope2 = scope1.ServiceProvider.CreateScope())
+        {
+            IFooSingleton fooService2 = scope2.ServiceProvider.GetService<IFooSingleton>()!;
+            IBarScoped barService2 = scope2.ServiceProvider.GetService<IBarScoped>()!;
+            
+            Console.WriteLine("New Scope 2");
+            Console.WriteLine(fooService2.GetName());
+            Console.WriteLine(barService2.GetName());
+        }
         
         IServiceCollection newServiceCollection = new ServiceCollection();
         foreach (ServiceDescriptor serviceDescriptor in provider.GetRequiredService<IServiceCollection>().AsEnumerable())
             newServiceCollection.Add(serviceDescriptor);
         IServiceProvider newServiceProvider = newServiceCollection.BuildServiceProvider();
         
-        IFooSingleton fooService2 = newServiceProvider.GetService<IFooSingleton>()!;
-        IBarScoped barService2 = newServiceProvider.GetService<IBarScoped>()!;
+        IFooSingleton fooService3 = newServiceProvider.GetService<IFooSingleton>()!;
+        IBarScoped barService3 = newServiceProvider.GetService<IBarScoped>()!;
         
         Console.WriteLine("New Service provider");
-        Console.WriteLine(fooService2.GetName());
-        Console.WriteLine(barService2.GetName());
+        Console.WriteLine(fooService3.GetName());
+        Console.WriteLine(barService3.GetName());
+        Console.WriteLine("----------\n");
+
+        
+        // IServiceCollection spCollection = provider.GetRequiredService<IServiceCollection>();
+        // IServiceProvider newServiceProviderWithSingletonCopy = ServiceCollectionContainerBuilderExtensions.BuildServiceProvider(spCollection, true);
+        //
+        // IFooSingleton fooService4 = newServiceProviderWithSingletonCopy.GetService<IFooSingleton>()!;
+        // IBarScoped barService4 = newServiceProviderWithSingletonCopy.GetService<IBarScoped>()!;
+        //
+        // Console.WriteLine("New Service provider with copy of singletons");
+        // Console.WriteLine(fooService4.GetName());
+        // Console.WriteLine(barService4.GetName());
+        // Console.WriteLine("----------\n");
+        
     }
 }).WithName("RunTest");
 
